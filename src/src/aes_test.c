08c9976b102efbf2d0c38c688690d11c9ce2c253
root <root@localhost.(none)>
2016-02-03 02:00:44 -0800

    rm

--- src/src/aes_test.c
+++
@@ -1,5 +0,0 @@
-#include "aes.h"
-
-int main(int argc, char *argv[]) {
-    return 0;
-}
--- src/src/version.h
+++
@@ -1,6 +0,0 @@
-#ifndef _VERSION_INCLUDED
-#define _VERSION_INCLUDED 1
-
-#define _VERSION "encry-0.0.1"
-
-#endif
